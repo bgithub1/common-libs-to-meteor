538da24490f4fd55b834d972bbe7f09239bef97e
henry perlman <bperlman1@gmail.com>
2015-07-08 01:36:39 -0400

    debug TableChangedByUser logic

---
+++ src/main/java/com/billybyte/commonlibstometeor/runs/RunTableChangedSubscribe.java
@@ -0,0 +1,9 @@
+package com.billybyte.commonlibstometeor.runs;
+
+import com.billybyte.meteorjava.runs.SetupTableModelsAndSendReceiveLists;
+
+public class RunTableChangedSubscribe {
+	public static void main(String[] args) {
+		SetupTableModelsAndSendReceiveLists.main(args);
+	}
+}
