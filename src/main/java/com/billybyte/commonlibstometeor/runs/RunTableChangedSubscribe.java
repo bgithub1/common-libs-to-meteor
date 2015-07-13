d4414726fc1ea47d88d4ac986972119948e1b03e
henry perlman <bperlman1@gmail.com>
2015-07-12 23:07:39 -0400

    deleted files removed

--- src/main/java/com/billybyte/commonlibstometeor/runs/RunTableChangedSubscribe.java
+++
@@ -1,9 +0,0 @@
-package com.billybyte.commonlibstometeor.runs;
-
-import com.billybyte.meteorjava.runs.SetupTableModelsAndSendReceiveLists;
-
-public class RunTableChangedSubscribe {
-	public static void main(String[] args) {
-		SetupTableModelsAndSendReceiveLists.main(args);
-	}
-}
