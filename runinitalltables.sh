216726ae6d1c54e8ea68082f84043f6789c536be
henry perlman <bperlman1@gmail.com>
2015-07-28 17:43:18 -0400

    ${*:1}

--- runinitalltables.sh
+++ runinitalltables.sh
@@ -1,12 +1,20 @@
-sh runInNewTab.sh ./ mvnexec.sh com.billybyte.commonlibstometeor.runs.initializers.RunInitializeMeteorPosition "metUrl=localhost" "metPort=3000" "adminEmail=admin1@demo.com" "adminPass=admin1"
-sleep 4
-sh runInNewTab.sh ./ mvnexec.sh com.billybyte.commonlibstometeor.runs.initializers.RunInitializeJnestShortNameList "metUrl=localhost" "metPort=3000" "adminEmail=admin1@demo.com" "adminPass=admin1"  "dseXmlPath= beans_DseFromMongoBasedQm_EvalToday.xml"
-sleep 4
-sh runInNewTab.sh ./ mvnexec.sh com.billybyte.commonlibstometeor.runs.initializers.RunInitializeMeteorGreeksData "metUrl=localhost" "metPort=3000" "adminEmail=admin1@demo.com" "adminPass=admin1"
-sleep 4
-sh runInNewTab.sh ./ mvnexec.sh com.billybyte.commonlibstometeor.runs.initializers.RunInitializeMeteorProfitAndLoss "metUrl=localhost" "metPort=3000" "adminEmail=admin1@demo.com" "adminPass=admin1"
-sleep 4
-sh runInNewTab.sh ./ mvnexec.sh com.billybyte.commonlibstometeor.runs.initializers.RunInitializeMeteorUnitVar "metUrl=localhost" "metPort=3000" "adminEmail=admin1@demo.com" "adminPass=admin1"
-sleep 4
-sh runInNewTab.sh ./ mvnexec.sh com.billybyte.commonlibstometeor.runs.initializers.RunInitializeMeteorGreekInputsData "metUrl=localhost" "metPort=3000" "adminEmail=admin1@demo.com" "adminPass=admin1"
-
+echo '******************************* RunInitializeDeleteAllExampleTableModels ************************************************'
+sh mvnexec.sh com.billybyte.commonlibstometeor.runs.initializers.RunInitializeDeleteAllTableModels "metUrl=localhost" "metPort=3000" "adminEmail=admin1@demo.com" "adminPass=admin1"  "dseXmlPath= beans_DseFromMongoBasedQm_EvalToday.xml"
+sleep 2
+echo '******************************* RunInitializeJnestShortNameList ************************************************'
+sh mvnexec.sh com.billybyte.commonlibstometeor.runs.initializers.RunInitializeJnestShortNameList "metUrl=localhost" "metPort=3000" "adminEmail=admin1@demo.com" "adminPass=admin1"  "dseXmlPath= beans_DseFromMongoBasedQm_EvalToday.xml"
+sleep 2
+echo '******************************* RunInitializeMeteorPosition ************************************************'
+sh mvnexec.sh com.billybyte.commonlibstometeor.runs.initializers.RunInitializeMeteorPosition "metUrl=localhost" "metPort=3000" "adminEmail=admin1@demo.com" "adminPass=admin1"
+sleep 2
+echo '******************************* RunInitializeMeteorGreeksData ************************************************'
+sh mvnexec.sh com.billybyte.commonlibstometeor.runs.initializers.RunInitializeMeteorGreeksData "metUrl=localhost" "metPort=3000" "adminEmail=admin1@demo.com" "adminPass=admin1"
+sleep 2
+echo '******************************* RunInitializeMeteorProfitAndLoss ************************************************'
+sh mvnexec.sh com.billybyte.commonlibstometeor.runs.initializers.RunInitializeMeteorProfitAndLoss "metUrl=localhost" "metPort=3000" "adminEmail=admin1@demo.com" "adminPass=admin1"
+sleep 2
+echo '******************************* RunInitializeMeteorProfitAndLoss ************************************************'
+sh mvnexec.sh com.billybyte.commonlibstometeor.runs.initializers.RunInitializeMeteorUnitVar "metUrl=localhost" "metPort=3000" "adminEmail=admin1@demo.com" "adminPass=admin1"
+sleep 2
+echo '******************************* RunInitializeMeteorGreekInputsData ************************************************'
+sh mvnexec.sh com.billybyte.commonlibstometeor.runs.initializers.RunInitializeMeteorGreekInputsData "metUrl=localhost" "metPort=3000" "adminEmail=admin1@demo.com" "adminPass=admin1"
---
+++ src/main/java/com/billybyte/commonlibstometeor/runs/initializers/RunInitializeDeleteAllTableModels.java
@@ -0,0 +1,43 @@
+package com.billybyte.commonlibstometeor.runs.initializers;
+
+import java.net.URISyntaxException;
+import java.util.HashMap;
+import java.util.List;
+
+import com.billybyte.commonlibstometeor.runs.ArgBundle;
+import com.billybyte.meteorjava.MeteorListSendReceive;
+import com.billybyte.meteorjava.MeteorTableModel;
+import com.billybyte.meteorjava.runs.SetupTableModelsAndSendReceiveLists;
+import com.billybyte.meteorjava.staticmethods.Utils;
+
+public class RunInitializeDeleteAllTableModels {
+	public static void main(String[] args) {
+		ArgBundle ab = new ArgBundle(args);
+		
+		
+		@SuppressWarnings("unchecked")
+		List<MeteorTableModel> mtmList = Utils.getFromXml(List.class, SetupTableModelsAndSendReceiveLists.class, "positionMeteorTableModels.xml");
+		
+		MeteorListSendReceive<MeteorTableModel> mlsrTableModel=null;
+		try {
+			mlsrTableModel = new MeteorListSendReceive<MeteorTableModel>(100, 
+					MeteorTableModel.class, ab.meteorUrl, ab.meteorPort, 
+					ab.adminEmail,ab.adminPass,"", "", "tester");
+		} catch (URISyntaxException e) {
+			throw Utils.IllState(e);
+		}
+		// the mtmList only has one object, but I put in an iteration
+		 //  for future use.
+		mtmList = mlsrTableModel.getList(new HashMap<String, String>());
+		for(MeteorTableModel mtm:mtmList){
+			String tableId = mtm.getId();
+			String error = mlsrTableModel.deleteMeteorTableModel( tableId);
+			if(error!=null && error.compareTo("0")!=0){
+				Utils.prtObErrMess(MeteorTableModel.class, error);
+			}
+		}
+		
+		System.exit(0);
+
+	}
+}
