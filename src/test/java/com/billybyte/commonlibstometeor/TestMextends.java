81aa9f1cd0ed2d7186e69d30a24466b8e4503363
henry perlman <bperlman1@gmail.com>
2015-08-22 14:29:31 -0400

    add SerialUnderlyingQueries for OJ and SB - more to follow

--- src/test/java/com/billybyte/commonlibstometeor/TestMextends.java
+++ src/test/java/com/billybyte/commonlibstometeor/TestMextends.java
@@ -1,5 +1,15 @@
 package com.billybyte.commonlibstometeor;
 
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+
+import com.billybyte.commonstaticmethods.CollectionsStaticMethods;
+import com.billybyte.commonstaticmethods.Utils;
+import com.billybyte.dse.DerivativeSetEngine;
+import com.billybyte.dse.queries.SerialOptUnderQuery;
+import com.billybyte.marketdata.SecDef;
+
 import junit.framework.TestCase;
 
 public class TestMextends extends TestCase {
@@ -20,5 +30,41 @@ public class TestMextends extends TestCase {
 	public void test1(){
 		ClassSub cs = new ClassSub();
 		cs.myMethod(10);
+	}
+	
+	public void testSerialOptUnderQueries(){
+		SerialOptUnderQuery sBSerialOptUnderQuery = Utils.springGetBean(SerialOptUnderQuery.class, 
+				"beans_MongoBasedQueryManager.xml", "sbUnderQuery");
+		assertNotNull(sBSerialOptUnderQuery);
+		List<SecDef> sbSdList = 
+				sBSerialOptUnderQuery.get("SB.FOP.NYBOT.USD.201511.C.12.00", 1, TimeUnit.SECONDS);
+		assertNotNull(sbSdList);
+		assertTrue(sbSdList.size()>0);
+		CollectionsStaticMethods.prtListItems(sbSdList);
+
+		SerialOptUnderQuery ojSerialOptUnderQuery = Utils.springGetBean(SerialOptUnderQuery.class, 
+				"beans_MongoBasedQueryManager.xml", "ojUnderQuery");
+		assertNotNull(ojSerialOptUnderQuery);
+		List<SecDef> ojSdList = 
+				ojSerialOptUnderQuery.get("OJ.FOP.NYBOT.USD.201510.C.130.00", 1, TimeUnit.SECONDS);
+		assertNotNull(ojSdList);
+		assertTrue(ojSdList.size()>0);
+		CollectionsStaticMethods.prtListItems(ojSdList);
+	
+	}
+	
+	public void testSerialQueriesUsingQm(){
+		DerivativeSetEngine dse = Utils.springGetBean(DerivativeSetEngine.class, 
+				"beans_DseFromMongoBasedQm_EvalToday.xml", "dse");
+		assertNotNull(dse);
+		List<SecDef> sbSdList = dse.getQueryManager().getUnderlyingSecDefs("SB.FOP.NYBOT.USD.201511.C.12.00", 1, TimeUnit.SECONDS);
+		assertNotNull(sbSdList);
+		assertTrue(sbSdList.size()>0);
+		CollectionsStaticMethods.prtListItems(sbSdList);
+		
+		List<SecDef> ojSdList = dse.getQueryManager().getUnderlyingSecDefs("OJ.FOP.NYBOT.USD.201510.C.130.00", 1, TimeUnit.SECONDS);
+		assertNotNull(ojSdList);
+		assertTrue(ojSdList.size()>0);
+		CollectionsStaticMethods.prtListItems(ojSdList);
 	}
 }
