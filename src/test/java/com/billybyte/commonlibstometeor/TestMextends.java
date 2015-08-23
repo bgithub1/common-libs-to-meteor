815ae2c8c4a6960151ece36cb933da26ab786b01
henry perlman <bperlman1@gmail.com>
2015-08-23 11:48:01 -0400

    fix serial month processing and add serial month references to underlying queries in query manager spring xml files

--- src/test/java/com/billybyte/commonlibstometeor/TestMextends.java
+++ src/test/java/com/billybyte/commonlibstometeor/TestMextends.java
@@ -1,5 +1,6 @@
 package com.billybyte.commonlibstometeor;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
@@ -13,6 +14,29 @@ import com.billybyte.marketdata.SecDef;
 import junit.framework.TestCase;
 
 public class TestMextends extends TestCase {
+	// Array of shortNames that are "serial month" options
+	private static final String[] serialOptSnArr = {
+			"SB.FOP.NYBOT.USD.201511.C.12.00",
+			"OJ.FOP.NYBOT.USD.201510.C.130.00",
+			"ES.FOP.GLOBEX.USD.201511.C.1950",
+			"DX.FOP.NYBOT.USD.201510.C.100.00"
+	};
+	// Array of shortNames that are the expected underlyings of the  "serial month" options
+	private static final String[] expectedUnderSnArr = {
+			"SB.FUT.NYBOT.USD.201603",
+			"OJ.FUT.NYBOT.USD.201511",
+			"ES.FUT.GLOBEX.USD.201512",
+			"DX.FUT.NYBOT.USD.201512"
+	};
+	
+	
+	
+	@Override
+	protected void setUp() throws Exception {
+		// TODO Auto-generated method stub
+		super.setUp();
+	}
+
 	static abstract class ClassMain {
 		abstract <M extends ClassMain> M myMethod(int i);
 		int myInt;
@@ -57,14 +81,20 @@ public class TestMextends extends TestCase {
 		DerivativeSetEngine dse = Utils.springGetBean(DerivativeSetEngine.class, 
 				"beans_DseFromMongoBasedQm_EvalToday.xml", "dse");
 		assertNotNull(dse);
-		List<SecDef> sbSdList = dse.getQueryManager().getUnderlyingSecDefs("SB.FOP.NYBOT.USD.201511.C.12.00", 1, TimeUnit.SECONDS);
-		assertNotNull(sbSdList);
-		assertTrue(sbSdList.size()>0);
-		CollectionsStaticMethods.prtListItems(sbSdList);
+
+		List<SecDef> serialSdList = new ArrayList<SecDef>();
+		for(String serialOptSn : serialOptSnArr){
+			List<SecDef> sdList = dse.getQueryManager().getUnderlyingSecDefs(serialOptSn, 1, TimeUnit.SECONDS);
+			assertNotNull(sdList);
+			assertTrue(sdList.size()>0);
+			serialSdList.addAll(sdList);
+		}
+		// if you get here, all went well
+		CollectionsStaticMethods.prtListItems(serialSdList);
 		
-		List<SecDef> ojSdList = dse.getQueryManager().getUnderlyingSecDefs("OJ.FOP.NYBOT.USD.201510.C.130.00", 1, TimeUnit.SECONDS);
-		assertNotNull(ojSdList);
-		assertTrue(ojSdList.size()>0);
-		CollectionsStaticMethods.prtListItems(ojSdList);
+		// now check to see if what you got was what you expected
+		for(int i = 0;i<serialSdList.size();i++){
+			assertTrue(serialSdList.get(i).getShortName().compareTo(expectedUnderSnArr[i])==0);
+		}
 	}
 }
