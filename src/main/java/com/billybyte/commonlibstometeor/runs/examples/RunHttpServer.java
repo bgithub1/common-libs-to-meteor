ac48d2f60eb33434643524c641795a7eb0fb3c57
billybyte <bperlman@liverisk.com>
2015-08-29 15:08:57 -0400

    regex price vol query

--- src/main/java/com/billybyte/commonlibstometeor/runs/examples/RunHttpServer.java
+++ src/main/java/com/billybyte/commonlibstometeor/runs/examples/RunHttpServer.java
@@ -6,8 +6,10 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 import java.util.TreeMap;
+import java.util.Map.Entry;
+import java.util.Set;
+import java.util.TreeSet;
 import java.util.concurrent.TimeUnit;
 
 import com.billybyte.clientserver.httpserver.HttpCsvQueryServer;
@@ -18,26 +20,41 @@ import com.billybyte.commonstaticmethods.Utils;
 import com.billybyte.dse.DerivativeSetEngine;
 import com.billybyte.dse.debundles.DerivativeSetEngineBuilder;
 import com.billybyte.dse.inputs.InBlk;
-import com.billybyte.dse.inputs.diotypes.AtmDiot;
-import com.billybyte.dse.inputs.diotypes.SettlePriceDiot;
-import com.billybyte.dse.inputs.diotypes.VolDiot;
-import com.billybyte.dse.outputs.DerivativeReturn;
-import com.billybyte.dse.outputs.OptPriceDerSen;
+import com.billybyte.mongo.MongoDatabaseNames;
+import com.billybyte.mongo.MongoXml;
 import com.billybyte.queries.ComplexQueryResult;
 
 public class RunHttpServer {
+	/**
+	 * 
+	 * runtime params 
+mongoPriceVolUr=127.0.0.1 mongoPriceVolPort=27017 mongoSpanlUrl=127.0.0.1 
+mongoSpanPort=27022 
+mongoXmlSettleDbName=settleDb 
+mongoXmlSettleCollName=settleColl  
+mongoXmlVolDbName=ImpliedVolDb 
+mongoXmlVolCollName=ImpliedVolColl 
+		  
+		 
+
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		
 		// Get all of the usual arguments that pertain to connecting to meteor
 		ArgBundle ab = new ArgBundle(args);
+		
 		// Get the path of a Spring Xml file that will create a DerivativeSetEngine
 		
 		int httpPort = new Integer(ab.argPairs.get("httpPort"));
 		String httpPath = ab.argPairs.get("httpPath");
 		
-		// Create main processor that turns Position objects
+		
+		
+		// ****************** Create stuff for main csv query (getDseInputs) **********
 		//  into instances of GreeksData.
 		// you don't really use yahoo to get dse
+		
 		final DerivativeSetEngine dse = 
 				DerivativeSetEngineBuilder.dseForStocksUsingYahooAndSpring(null,ab.dseXmlPath);
 	
@@ -52,12 +69,48 @@ public class RunHttpServer {
 		} catch (IOException e) {
 			throw Utils.IllState(e);
 		}
-		httpServerQuery.addAlternativePath("/futpricevol", new MyFutPriceVolQuery(dse));
+		
+		// ******************** now create stuff fomr MyFutPriceVolQuery *************
+		String mongoPricVolUrl = ab.argPairs.get("mongoPriceVolUrl");
+		String mongoPriceVolPortString = ab.argPairs.get("mongoPriceVolPort");
+		int mongoPriceVolPort = mongoPriceVolPortString == null ? 27017 : new Integer(mongoPriceVolPortString);
+		String mongoSpanlUrl = ab.argPairs.get("mongoSpanlUrl");
+		String mongoSpanPortString = ab.argPairs.get("mongoSpanPort");
+		int mongoSpanPort = mongoPriceVolPortString == null ? 27017 : new Integer(mongoSpanPortString);
+
+		// mongoXmlSettle
+		String mongoXmlSettleDbName = ab.argPairs.get("mongoXmlSettleDbName");
+		if(mongoXmlSettleDbName==null){
+			mongoXmlSettleDbName = MongoDatabaseNames.SETTLEMENT_DB;
+		}
+		
+		String mongoXmlSettleCollName = ab.argPairs.get("mongoXmlSettleCollName");
+		if(mongoXmlSettleCollName==null){
+			mongoXmlSettleCollName = MongoDatabaseNames.SETTLEMENT_CL;
+		}
+		MongoXml<SettlementDataInterface> mongoXmlSettle = 
+				new MongoXml<SettlementDataInterface>(mongoPricVolUrl, mongoPriceVolPort, mongoXmlSettleDbName, mongoXmlSettleCollName);
+		// end mongoXmlSettle
+
+		// mongoXmlVol
+		String mongoXmlVolDbName = ab.argPairs.get("mongoXmlVolDbName");
+		if(mongoXmlVolDbName==null){
+			mongoXmlVolDbName = MongoDatabaseNames.IMPLIEDVOL_DB;
+		}
+		
+		String mongoXmlVolCollName = ab.argPairs.get("mongoXmlVolCollName");
+		if(mongoXmlVolCollName==null){
+			mongoXmlVolCollName = MongoDatabaseNames.IMPLIEDVOL_CL;
+		}
+		MongoXml<BigDecimal> mongoXmlVol = 
+				new MongoXml<BigDecimal>(mongoPricVolUrl, mongoPriceVolPort, mongoXmlVolDbName, mongoXmlVolCollName);
+		// end mongoXmlSettle
+		httpServerQuery.addAlternativePath("/futpricevol", new MyFutPriceVolQuery(mongoXmlSettle,mongoXmlVol));
+		
 		httpServerQuery.start();
 	}
 	
 	static class MyGreeksInputQuery implements QueryInterface<String, List<String[]>> {
-
 		private final DerivativeSetEngine dse;
 		private MyGreeksInputQuery(DerivativeSetEngine dse){
 			this.dse = dse;
@@ -101,10 +154,12 @@ public class RunHttpServer {
 	
 	
 	static class MyFutPriceVolQuery implements QueryInterface<String, List<String[]>> {
-
-		private final DerivativeSetEngine dse;
-		private MyFutPriceVolQuery(DerivativeSetEngine dse){
-			this.dse = dse;
+		private final MongoXml<SettlementDataInterface> mongoXmlSettle;
+		MongoXml<BigDecimal> mongoXmlVol;
+		private MyFutPriceVolQuery(MongoXml<SettlementDataInterface> mongoXmlSettle,
+				MongoXml<BigDecimal> mongoXmlVol){
+			this.mongoXmlSettle = mongoXmlSettle;
+			this.mongoXmlVol = mongoXmlVol;
 		}
 		@Override
 		public List<String[]> get(String qparams, int timeoutValue,
@@ -114,33 +169,48 @@ public class RunHttpServer {
 			// the qparams string has the http parameters.
 			//   
     		String[] matches = qparams.split("&");
-    		Set<String> derivativeShortNameSet = new HashSet<String>();
+    		Set<String> regexDerivNames = new TreeSet<String>();
     		for(String match: matches){
     			String[] pair = match.split("=");
-    			derivativeShortNameSet.add(pair[1]);
+    			regexDerivNames.add(pair[1]);
     		}
-    		Map<String, ComplexQueryResult<InBlk>> inputBlkCqrMap = 
-					dse.getInputs(derivativeShortNameSet);
 			List<String[]> outputCsv = new ArrayList<String[]>();
 			String[] header = {"shortName","settle","settleVol"};
 			outputCsv.add(header);
-    		for(String sn : derivativeShortNameSet){
-    			ComplexQueryResult<InBlk> inBlkCqr = inputBlkCqrMap.get(sn);
-    			String[] newLine = new String[3];
-				newLine[0] = sn;
-    			if(inBlkCqr!=null && inBlkCqr.isValidResult()){
-    				InBlk inBlk = inBlkCqr.getResult();
-    				SettlementDataInterface settle = inBlk.getMainInputList(new SettlePriceDiot());
-    				if(settle!=null && settle.getPrice()!=null){
-    					newLine[1] = settle.getPrice().toString();
+			Map<String, String[]> outputMap = 
+					new TreeMap<String, String[]>();
+			
+			for(String regexName : regexDerivNames){
+    			Map<String, SettlementDataInterface> settles = 
+    					mongoXmlSettle.getByRegex(regexName);
+    			Map<String, BigDecimal> vols = 
+    					mongoXmlVol.getByRegex(regexName);
+    			for(Entry<String, SettlementDataInterface> entry : settles.entrySet()){
+    				String sn = entry.getKey();
+    				if(!outputMap.containsKey(sn)){
+    					outputMap.put(sn, new String[]{sn,"",""});
     				}
-    				BigDecimal vol = inBlk.getMainInputList(new VolDiot());
-    				if(vol!=null){
-    					newLine[2] = vol.toString();
+    				SettlementDataInterface settle = 
+    						entry.getValue();
+    				if(settle!=null && settle.getPrice()!=null){
+    					String[] output = outputMap.get(sn);
+    					output[1] = settle.getPrice().toString();
     				}
     			}
-    			outputCsv.add(newLine);
-    		}
+    			for(Entry<String, BigDecimal> entry : vols.entrySet()){
+    				String sn = entry.getKey();
+    				BigDecimal vol = entry.getValue();
+    				if(vol!=null){
+    					String[] output = outputMap.get(sn);
+    					output[2] = vol.toString();
+    				}
+    			}
+				
+			}
+			
+			for(String sn : outputMap.keySet()){
+				outputCsv.add(outputMap.get(sn));
+			}
     		
 			return outputCsv;
 		}
